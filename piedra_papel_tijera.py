3fbf00490718ef0bf8b9f000eb05356b7f034ace
kesant <kevin_santana120@hotmail.com>
2021-05-13 00:27:50 -0500

    mi version del juego piedras papel o ptijeras

---
+++ piedra_papel_tijera.py
@@ -0,0 +1,39 @@
+def ganador (jugador1,jugador2):#funcion que elije el ganador en una ronda
+    victoria=(jugador1,jugador2)
+    gano=[("piedra","tijera"),("tijera","papel"),("papel,piedra")]
+    perdio=[("tijera","piedra"),("papel","tijera"),("piedra,papel")]
+    if victoria in gano:
+        return "jugador1"
+    else:
+        return "jugador2"  
+
+def proceso():#elije el ganador del juego
+    opciones=["piedra","papel","tijera"]
+    puntuacion=[]
+    for intentos in range(3):
+        i=0
+        while i==0:
+            jugador1=input("selecione su opcion jugador 1:")
+            jugador2=input("selecione su opcion jugador 2 :")
+            if (jugador1 in opciones) and (jugador2 in opciones):
+                 i=1
+            else:
+                print("Deben ingresar una opcion valida !!!!!")    
+        if ganador(jugador1,jugador2)=="jugador1":
+            print("El jugador 1 gano esta ronda")
+            puntuacion.append(1)  
+        else:
+            print("EL jugador 2 gano esta ronda") 
+            puntuacion.append(2)    
+    if puntuacion.count(1)>=2:
+        print("El jugador 1 gano el juego")  
+    else :
+        print("El jugador 2 gano el juego")    
+    
+def ingreso():
+    print(" Piedra,papel o tijera")
+    proceso()
+
+
+if __name__=="__main__":
+    ingreso()
