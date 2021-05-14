c21bd2161a941360590e4ff6bf6b5e62c0a59249
kesant <kevin_santana120@hotmail.com>
2021-05-14 14:13:56 -0500

    finalizamos el reto 5

--- Rangos_cambiantes.py
+++ Rangos_cambiantes.py
@@ -1,5 +1,13 @@
 def main():
-    pass
-
+   limite_inferior=float(input("Ingrese el limite inferior: "))
+   limite_superior=float(input("Ingrese el limite superior: "))
+   comparador=float(input( "ingrese el numero a comparar: "))
+   if comparador>=limite_inferior and comparador<=limite_superior:
+       print("**************************************************************")
+       print("tu numero esta dentro del rango establecido")
+       print("El numero ",comparador, "esta dentro del limite")
+   else:
+       print("**************************************************************")
+       print("El numero no esta dentro del limite")
 if __name__=="__main__":
     main()
