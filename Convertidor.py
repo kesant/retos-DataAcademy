2c12886d80e49c45a973d3f1cd39c3e2a0d155e6
kesant <kevin_santana120@hotmail.com>
2021-05-13 21:34:04 -0500

    hicimos el reto 3 del dataAcademy|

---
+++ Convertidor.py
@@ -0,0 +1,8 @@
+def menu():
+    print("""selecione una opcion :
+    opcion 1: convertir de millas a kilometros
+    opcion 2: convertir de kilometros a millas
+    """)
+
+if  __name__=="__main__":
+    menu()
